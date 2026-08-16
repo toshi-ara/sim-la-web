2882aae2998cf391afeba23ac04600da7f99faaf
ARA Toshiaki <toshiaki.ara@gmail.com>
2026-08-16 15:19:58 +0900

    add icons for PWA

--- images/svg2png.sh
+++ images/svg2png.sh
@@ -3,6 +3,10 @@
 inkscape back1.svg -w 497 -o back1.png
 inkscape back2.svg -w 497 -o back2.png
 
+inkscape favicon.svg -w 192 -o icon-192.png
+inkscape favicon.svg -w 512 -o icon-512.png
+
+
 convert -background none -density 256 favicon.svg \
   -define icon:auto-resize=16,32,48,64,128,256 favicon.ico
 
--- vite.config.js
+++ vite.config.js
@@ -23,6 +23,16 @@ export default defineConfig({
             "type": "image/x-icon"
           },
           {
+            "src": "icons/icon-192.png",
+            "sizes": "192x192",
+            "type": "image/png"
+          },
+          {
+            "src": "icons/icon-512.png",
+            "sizes": "512x512",
+            "type": "image/png"
+          },
+          {
             "src": "assets/back1.png",
             "sizes": "497x301",
             "type": "image/png",
